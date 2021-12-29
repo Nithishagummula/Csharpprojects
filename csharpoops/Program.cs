94d2236085f1c9683135abb837365635c653fc78
Nithishagummula <18j45a0412@gmail.com>
2021-12-29 15:51:30 +0530

    program file added

---
+++ csharpoops/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace hand
+{
+    class GetDetails
+    {
+        public void Details()
+        {
+            string path = @"C:\\visual\\networklog.txt";
+            FileStream objfstream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(objfstream);
+            Console.WriteLine("id\t\tSource\t\t\tDestination\t\tDate\t\t\tStatus\t\tNetwork");
+            while (sr.Peek() > 0)
+            {
+                string readmyline = sr.ReadLine();
+                string[] strings = readmyline.Split(':');
+                if (strings.Length > 1)
+                {
+                    Console.Write(strings[1]+"\t\t");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            GetDetails GetDetailsObj = new GetDetails();
+            GetDetailsObj.Details();
+            Console.ReadKey();
+        }
+    }
+}
+
